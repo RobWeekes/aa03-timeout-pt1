b5dcff8824ac6127e32e6e02aa9c54523612a446
robweekes <weekes.robert@gmail.com>
2024-04-15 19:11:05 -0700

    problem 4 done with 2 timers

--- problems/04-interval-count.js
+++ problems/04-interval-count.js
@@ -1,12 +1,12 @@
 /***********************************************************************
-Write a function, `intervalCount`, that accepts a callback, a delay in 
-milliseconds, and an amount. The function should set an interval with 
+Write a function, `intervalCount`, that accepts a callback, a delay in
+milliseconds, and an amount. The function should set an interval with
 the given callback and delay, but clear the interval after the callback
 has been executed 'amount' number of times.
 
 Hint: utilize a 'closure' to your advantage
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
 
 Example
@@ -16,13 +16,34 @@ intervalCount(function() {
 }, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
 ***********************************************************************/
 
+let count = 0;
 function intervalCount(cb, delay, amount) {
-  // Your code here 
-}
+    // invoke the callback "amount" number of times
+    // let timeoutID = setTimeout(cb, delay);
+    let timeoutID = setInterval(cb, delay);
+
+    // console.log(timeoutID);
+    let waitToStop = delay * amount;
+    console.log('start the interval');
+
+    // separate timer stops the first timer
+    setTimeout(() => {
+        console.log('end the interval');
+        clearInterval(timeoutID)
+    }, waitToStop + 5);             // wait for 1500 ms to terminate the interval method
+                                    // plus an extra 5ms buffer
+  }
+    //utilize a 'closure' to your advantage?
+
+intervalCount(function() {
+    console.log('hi');
+    count++;
+}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = intervalCount;
 } catch {
-  module.exports = null;
+  module.exports = null;a
 }
